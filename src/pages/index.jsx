f7592a31a546eeffc087839af7e0fc05c9a4da48
ShinEndo <endo@elplanning.co.jp>
2023-06-28 08:37:28 +0900

    ✨ 挙動確認用のuseEffectを導入

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,10 +2,19 @@ import Head from "next/head";
 import { Inter } from "next/font/google";
 import { Main } from "@/src/components/Main";
 import { Header } from "@/src/components/Header";
+import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+
+  useEffect(() => {
+    // DOMに直接触るのは、Reactでは基本的にNG（今回は例外）
+    document.body.style.backgroundColor = "lightblue";
+    return () => {
+          document.body.style.backgroundColor = "";
+    }
+  },[]);
   return (
     <>
       <Head>
