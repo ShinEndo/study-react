f804cdab19c5cc7869b90349eee7a0f160eefe3e
ShinEndo <endo@elplanning.co.jp>
2023-06-28 08:52:33 +0900

    ✨ カウンターを設置して、useStateでカウントアップする

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,11 +2,26 @@ import Head from "next/head";
 import { Inter } from "next/font/google";
 import { Main } from "@/src/components/Main";
 import { Header } from "@/src/components/Header";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+
+  const [count,setCount] = useState(1);
+
+  const handleClick = (e) => {
+    // ①と②の挙動は全く違う. useStateのset関数で直前の値を使う場合は、引数で受け取るようにする
+    
+    // ①
+    setCount(count => count + 1);
+    setCount(count => count + 1);
+
+    // ②
+    // setCount(count + 1);
+    // setCount(count + 1);
+
+  }
 
   useEffect(() => {
     // DOMに直接触るのは、Reactでは基本的にNG（今回は例外）
@@ -15,12 +30,16 @@ export default function Home() {
           document.body.style.backgroundColor = "";
     }
   },[]);
+
+
   return (
     <>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
+      <button onClick={handleClick}>ボタン</button>
+      <h1>{count}</h1>
       <Main title="index" />
     </>
   );
