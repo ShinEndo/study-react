3017e853a6d474cc504c9b9c75f953699c6b4432
ShinEndo <endo@elplanning.co.jp>
2023-06-29 08:52:04 +0900

    ⚡️ useCallback, useEffectの第2引数について

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import { Inter } from "next/font/google";
 import { Main } from "@/src/components/Main";
 import { Header } from "@/src/components/Header";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,18 +10,12 @@ export default function Home() {
 
   const [count,setCount] = useState(1);
 
-  const handleClick = (e) => {
-    // ①と②の挙動は全く違う. useStateのset関数で直前の値を使う場合は、引数で受け取るようにする
-    
-    // ①
-    setCount(count => count + 1);
-    setCount(count => count + 1);
-
-    // ②
-    // setCount(count + 1);
-    // setCount(count + 1);
-
-  }
+  const handleClick = useCallback((e) => {
+    if(count < 10) {
+      setCount(count => count + 1);
+    }
+  },[count]);
+  
 
   useEffect(() => {
     // DOMに直接触るのは、Reactでは基本的にNG（今回は例外）
